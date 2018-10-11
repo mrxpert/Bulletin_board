a27677ee7cbea4c1386c69a78a921ac3e6928ed7
mrxpert <mrxpert15@gmail.com>
2018-10-11 15:01:56 +0500

    Test for root page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
 end
